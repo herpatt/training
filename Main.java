079b94c0f5e851c32539f1f02862c386336b6c9e
herpatt <herpatt@users.noreply.github.com>
2018-04-26 22:14:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.hcl.example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee("Ponson");
+		emp1.displayEmployeeInformation();
+		
+		Employee emp2 = new Employee("Jenefa");
+		emp2.displayEmployeeInformation();
+		
+		Employee emp3 = new Employee("Joshua");
+		emp3.displayEmployeeInformation();		
+	}
+}
